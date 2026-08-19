4d2863468f4fd0f1023d5ef2bc13ca23ea966dec
leejm3 <leejm3@visang.com>
2026-08-20 08:53:44 +0900

    [FRONTEND] feat: 개별코칭 학생 스코프 조립 위젯 추가

--- frontend/src/widgets/coaching/index.ts
+++ frontend/src/widgets/coaching/index.ts
@@ -1,3 +1,4 @@
 export { CoachingOverviewSection } from './CoachingOverviewSection';
 export { ClassCoachingSection } from './ClassCoachingSection';
+export { IndividualCoachingSection } from './IndividualCoachingSection';
 export * from './Timeline';
