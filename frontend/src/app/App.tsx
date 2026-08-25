d6af2c8308b7387e912a971cc666d95d35f7fcc4
leejm3 <leejm3@visang.com>
2026-08-25 15:50:19 +0900

    Revert "[FRONTEND] refactor: Suspensive 에러 경계 도입"
    This reverts commit d88b635f178b83c6056e027362942b9320e8380e.

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,15 +1,12 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AppProviders } from './providers';
 import { AppRoutes } from './router/routes';
-import { RouteErrorBoundary } from './router/RouteErrorBoundary';
 
 export const App = () => {
   return (
     <AppProviders>
       <BrowserRouter>
-        <RouteErrorBoundary>
-          <AppRoutes />
-        </RouteErrorBoundary>
+        <AppRoutes />
       </BrowserRouter>
     </AppProviders>
   );
